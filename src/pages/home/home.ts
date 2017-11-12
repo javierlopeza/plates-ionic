947191aa297e06f0b3f4e7b510e2af921f23832e
Javier López <jilopez8@uc.cl>
2017-11-11 21:59:39 -0300

    List, add, remove plates. Set proxy for CORS.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,71 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  plates: any = [];
 
+  constructor(public navCtrl: NavController, public http: HttpClient, public alertCtrl: AlertController) {
+    this.getPlates();
   }
+
+  // GET all plates
+  getPlates() {
+    this.http.get('/api').subscribe(response => {
+      console.log(response);      
+      this.plates = response;
+    });
+  }
+
+  // DELETE plate
+  deletePlate(plate) {
+    for (let i = 0; i < this.plates.length; i++) {
+      if (this.plates[i] == plate) {
+        this.plates.splice(i, 1);
+        break;
+      }
+    }
+    this.http.get(`/api/del/${plate}`).subscribe(response => {
+      console.log(response);
+      this.getPlates();
+    });
+  }
+
+  // Input to create new plate
+  presentPrompt() {
+    let alert = this.alertCtrl.create({
+      title: 'New Plate',
+      inputs: [
+        {
+          name: 'plate',
+          placeholder: 'GLST13'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Add',
+          handler: data => {
+            this.createPlate(data.plate);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  // CREATE plate
+  createPlate(plate) {
+    this.http.get(`/api/new/${plate}`).subscribe(response => {
+      console.log(response);
+      this.getPlates();
+    });
+  }
+
+
 
 }
